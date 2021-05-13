93dc86c9e83bc65941d69d6e3bc2b52d15a5635b
A H Emtiaz <1905113@ugrad.cse.buet.ac.bd>
2021-05-13 12:11:02 +0600

    Deploying

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const userRouter = require('./routes/userRoute')
 const transactionRoutes = require('./routes/transactionRoutes')
+const path = require('path')
 
 const app = express()
 
@@ -19,10 +20,17 @@ app.use('/api/transactions',transactionRoutes)
 app.use(passport.initialize())
 require('./passport')(passport)
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT? process.env.PORT : 4000
 app.listen(PORT,()=>{
     console.log(`Server Running at ${PORT} port`)
-    mongoose.connect('mongodb://localhost/hal-khata', {useNewUrlParser: true, useUnifiedTopology: true},()=>{
+    mongoose.connect(`mongodb+srv://${process.env.dbUser}:${process.env.dbPassword}@hal-khata.qf3mg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true},()=>{
         console.log("Database Connected")
     });
 })
